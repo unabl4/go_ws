37f0b887696075d5b370a62cb7702f2d69e8b6a4
Dmitri <unabl4@gmail.com>
2019-03-07 15:34:59 +0200

    final api json response, wip

--- hw5_codegen/api_handlers.go
+++ hw5_codegen/api_handlers.go
@@ -1,27 +1,55 @@
 package main
 
 import (
-	// "bytes"
+	"encoding/json"
 	"net/http"
 )
 
 // ----
 
-// 1) receive incoming parameters
-// 2) parse into appropriate structure
-// 3) validate according to the structure -> reject if invalid
-// 4) 
+// - receive incoming parameters
+// - check the request method (GET/POST)
+// - parse into appropriate structure (json -> structure)
+// - validate according to the structure -> reject if invalid
 
-func userCreateHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
-}
+// ---
 
-func (srv *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	mux := http.NewServeMux()	// the first mux
-	mux.HandleFunc("/create/", func (w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello World"))
-	})
+type ApiResponse struct {
+	Error string 		 `json:"error"`
+	Response interface{} `json:"response,omitempty"`	// any inner structure
 }
 
 // ---
 
+func (srv *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "Request: " + r.URL.Path)
+
+	ctx := r.Context()	// request context
+	params := CreateParams { }
+	// TODO: Structure validation
+	// the main call
+	newUser, err := srv.Create(ctx, params)
+
+	if err != nil {
+		// error
+		e := err.(ApiError)	// type assertion; convert to the ~correct type (ApiError struct is allowed to be used)
+		w.WriteHeader(e.HTTPStatus)	// -> correct HTTP error status code
+
+		r := ApiResponse { e.Err.Error(), nil }	// new api response
+		j, err := json.Marshal(r) // -> json
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return		
+		}
+		w.Write(j)
+	} else {
+		// success
+		r := ApiResponse { "", newUser } // blank error to be present inside
+		j, err := json.Marshal(r)	// -> json
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return		
+		}
+		w.Write(j)
+	}
+}
