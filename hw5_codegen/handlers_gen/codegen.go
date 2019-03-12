f6c429b15afebf50f86a8e715d521bf6c5567011
Dmitri <unabl4@gmail.com>
2019-03-12 16:42:27 +0200

    field parsing (tags -> validators, default value, ref), code formatting, muting logs; redefine validators; wip

--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -8,7 +8,14 @@ import (
 	"go/token"
 	"log"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
+)
+
+var (
+	paramsPrefix = "p"             // something we agree on
+	tagPrefix    = "`apivalidator" // has to be with the tick
 )
 
 type ApiEntry struct {
@@ -29,7 +36,7 @@ func parseApiEntry(apiEntryDescriptor string) ApiEntry {
 // ---
 
 type Field struct {
-	Name            string      // name of the field (raw name)
+	Name            string      // name of the field (raw, original name)
 	Type            string      // 'int' or 'string'
 	DefaultValue    interface{} // interface (NULLABLE)
 	Validators      []Validator // list of validators
@@ -52,8 +59,7 @@ func (f Field) srcQueryParam() string {
 // ---
 
 type Validator interface {
-	IsValid(input interface{}) bool
-	// RenderError(fieldName string) string
+	Render(Field) string // display
 }
 
 type PresenceValidator struct{} // empty (no fields needed)
@@ -70,51 +76,68 @@ type EnumValidator struct {
 	AcceptedValues []string
 }
 
-func (v PresenceValidator) IsValid(input interface{}) bool {
-	if s, ok := input.(string); ok { // string
-		return len(s) > 0 // <> "" (not equal to an empty string)
+func (v PresenceValidator) Render(f Field) string {
+	if f.Type == "string" {
+		return fmt.Sprintf("if len(%s.%s) <= 0 {\n\treturn fmt.Errorf(\"%s must me not empty\")\n}", paramsPrefix, f.Name, f.srcQueryParam())
+	} else if f.Type == "int" {
+		return fmt.Sprintf("if %s.%s == 0 {\n\treturn fmt.Errorf(\"%s must me not empty\")\n}", paramsPrefix, f.Name, f.srcQueryParam())
+	} else {
+		// should NOT happen
+		panic("unsupported type")
 	}
-
-	if i, ok := input.(int); ok { // int
-		return i != 0 // by definition
-	}
-
-	// supposedly, ref types (pointers, maps, etc)
-	return input != nil
 }
 
-func (v MinValidator) IsValid(input interface{}) bool {
-	if s, ok := input.(string); ok { // string
-		return len(s) >= v.Value
-	}
-
-	if i, ok := input.(int); ok { // int
-		return i >= v.Value
-	}
-
-	return true
+func (v EnumValidator) Render(f Field) string {
+	return ""
 }
 
-func (v MaxValidator) IsValid(input interface{}) bool {
-	if s, ok := input.(string); ok { // string
-		return len(s) <= v.Value
-	}
-
-	if i, ok := input.(int); ok { // int
-		return i <= v.Value
-	}
-
-	return true
+func (v MinValidator) Render(f Field) string {
+	return ""
 }
 
-func (v EnumValidator) IsValid(input interface{}) bool {
-	for _, v := range v.AcceptedValues {
-		if v == input {
-			return true // item found -> valid
+func (v MaxValidator) Render(f Field) string {
+	return ""
+}
+
+// ---
+
+func parseField(fieldName string, fieldType string, fieldTag string) Field {
+	var validators []Validator
+	tagKeys := strings.Split(fieldTag, ",")
+
+	f := Field{}
+	f.Name = fieldName
+	f.Type = fieldType
+
+	for _, tagKey := range tagKeys { // process individual tag keys
+		split := strings.Split(tagKey, "=")
+
+		if split[0] == "required" {
+			validators = append(validators, PresenceValidator{})
+		} else if split[0] == "enum" {
+			vals := strings.Split(split[1], "|")
+			validators = append(validators, EnumValidator{vals})
+		} else if split[0] == "default" {
+			if fieldType == "string" {
+				f.DefaultValue = split[1]
+			} else if fieldType == "int" {
+				f.DefaultValue, _ = strconv.Atoi(split[1])
+			} else {
+				panic("unsupported type")
+			}
+		} else if split[0] == "paramname" {
+			f.SourceParamName = split[1]
+		} else if split[0] == "min" {
+			i, _ := strconv.Atoi(split[1]) // str -> int
+			validators = append(validators, MinValidator{i})
+		} else if split[0] == "max" {
+			i, _ := strconv.Atoi(split[1]) // str -> int
+			validators = append(validators, MaxValidator{i})
 		}
 	}
 
-	return false
+	f.Validators = validators
+	return f
 }
 
 // ===
@@ -155,13 +178,22 @@ func main() {
 					continue
 				}
 
-				fmt.Println("Struct parsing >", typeSpec.Name)
-				fmt.Println(typeSpec.Type)
-				for _, e := range currStruct.Fields.List {
-					fmt.Println(e.Type)
-					if e.Tag != nil {
-						fmt.Println(e.Tag.Value)
+				// fmt.Println("Struct parsing >", typeSpec.Name)
+				// fmt.Println(typeSpec.Type)
+				for _, field := range currStruct.Fields.List {
+					var fieldType, tagDescriptor string
+					switch field.Type.(type) {
+					case *ast.Ident:
+						fieldType = field.Type.(*ast.Ident).Name
 					}
+					// hopefully, other cases will not appear
+
+					if field.Tag != nil {
+						tagReflect := reflect.StructTag(field.Tag.Value)
+						tagDescriptor = tagReflect.Get(tagPrefix)
+					}
+					fl := parseField(field.Names[0].Name, fieldType, tagDescriptor)
+					fmt.Println(fl)
 				}
 			}
 
@@ -181,18 +213,18 @@ func main() {
 				continue
 			}
 
-			fmt.Println(f.Name, "/function params:")
+			// fmt.Println(f.Name, "/function params:")
 			for _, param := range f.Type.Params.List[1:] {
 				fmt.Println(param.Type)
 			}
 
 			for _, recv := range f.Recv.List {
-				fmt.Printf("recv type : %T", recv.Type)
+				// fmt.Printf("recv type : %T", recv.Type)
 
 				switch xv := recv.Type.(type) {
 				case *ast.StarExpr:
-					if si, ok := xv.X.(*ast.Ident); ok {
-						fmt.Println("*", si.Name)
+					if _, ok := xv.X.(*ast.Ident); ok {
+						// fmt.Println("*", si.Name)
 					}
 				case *ast.Ident:
 					// not this time anyway
