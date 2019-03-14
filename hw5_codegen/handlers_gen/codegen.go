60d72f7ebc7733afcd53c894235c366aa59722bf
Dmitri <unabl4@gmail.com>
2019-03-14 17:19:11 +0200

    major fixes: error handling, min/max string len error messages; application of the http request method checker middleware - not to be applied for 'apigen'-marked funcs w/o method stated explicitly ; fix the method name call - not static 'Create'; accepted

--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -36,7 +36,6 @@ func (e ApiEndpoint) handlerFuncName() string {
 
 func parseApiEndpoint(apiEndpointDescriptor string) ApiEndpoint {
 	e := ApiEndpoint{}
-	e.Method = "GET" // default method value
 	json.Unmarshal([]byte(apiEndpointDescriptor), &e)
 	return e
 }
@@ -127,7 +126,7 @@ func (v EnumValidator) Render(f Field) string {
 
 func (v MinValidator) Render(f Field) string {
 	if f.Type == "string" {
-		return fmt.Sprintf("if len(%s.%s) < %d {\n\treturn fmt.Errorf(\"%s must be >= %d\")\n}", muxPrefix, f.Name, v.Value, f.srcQueryParam(), v.Value)
+		return fmt.Sprintf("if len(%s.%s) < %d {\n\treturn fmt.Errorf(\"%s len must be >= %d\")\n}", muxPrefix, f.Name, v.Value, f.srcQueryParam(), v.Value)
 	} else if f.Type == "int" {
 		return fmt.Sprintf("if %s.%s < %d {\n\treturn fmt.Errorf(\"%s must be >= %d\")\n}", muxPrefix, f.Name, v.Value, f.srcQueryParam(), v.Value)
 	} else {
@@ -138,30 +137,13 @@ func (v MinValidator) Render(f Field) string {
 
 func (v MaxValidator) Render(f Field) string {
 	if f.Type == "string" {
-		return fmt.Sprintf("if len(%s.%s) > %d {\n\treturn fmt.Errorf(\"%s must be <= %d\")\n}", muxPrefix, f.Name, v.Value, f.srcQueryParam(), v.Value)
+		return fmt.Sprintf("if len(%s.%s) > %d {\n\treturn fmt.Errorf(\"%s len must be <= %d\")\n}", muxPrefix, f.Name, v.Value, f.srcQueryParam(), v.Value)
 	} else if f.Type == "int" {
 		return fmt.Sprintf("if %s.%s > %d {\n\treturn fmt.Errorf(\"%s must be <= %d\")\n}", muxPrefix, f.Name, v.Value, f.srcQueryParam(), v.Value)
 	} else {
 		// should NOT happen
 		panic("unsupported type")
 	}
-}
-
-// stringer
-func (v PresenceValidator) String() string {
-	return fmt.Sprintf("Must be present")
-}
-
-func (v EnumValidator) String() string {
-	return fmt.Sprintf("Must be one of: %s", strings.Join(v.AcceptedValues, ","))
-}
-
-func (v MinValidator) String() string {
-	return fmt.Sprintf("Min=%d", v.Value)
-}
-
-func (v MaxValidator) String() string {
-	return fmt.Sprintf("Max=%d", v.Value)
 }
 
 // ---
@@ -214,14 +196,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// out, _ := os.Create(os.Args[2]) // create the file
-
-	// write
-	// fmt.Fprintln(out, `package `+f.Name.Name) // package is required
-	// fmt.Fprintln(out)                            // empty line
-	// fmt.Fprintln(out, `import "encoding/binary"`)
-	// fmt.Fprintln(out, `import "bytes"`)
-	// fmt.Fprintln(out) // empty line
+	out, _ := os.Create(os.Args[2])   // create the file
+	fmt.Fprintln(out, `package main`) // ?
+	fmt.Fprintln(out)
+	fmt.Fprintln(out, `import "bytes"`)
+	fmt.Fprintln(out, `import "encoding/json"`)
+	fmt.Fprintln(out, `import "fmt"`)
+	fmt.Fprintln(out, `import "net/http"`)
+	fmt.Fprintln(out, `import "strconv"`)
+	fmt.Fprintln(out) // empty line
 
 	var routers = make(map[string][]ApiEndpoint)
 	var paramStructs = make(map[string]Params) // e.g "CreateParams" -> [F1,F2,...,Fn], where 'F' = Field 'instance'
@@ -324,39 +307,36 @@ func main() {
 		// fmt.Println("K=", k,v)
 		for _, e := range v {
 			// mux api endpoints
-			paramsName := e.Params[0].Name // ?
+			paramsName := e.Params[0].Name // hack
 
-			fmt.Printf("func (%s %s) %s(w http.ResponseWriter, r *http.Request) {\n", muxPrefix, k, e.handlerFuncName())
-			fmt.Println("\tctx := r.Context()") // context is always present as the first argument
-			fmt.Println("\tquery := r.URL.Query()")
-			fmt.Println() // newline spacer
-			fmt.Printf("\tparams := %s{}\n", paramsName)
+			fmt.Fprintf(out, "func (%s %s) %s(w http.ResponseWriter, r *http.Request) {\n", muxPrefix, k, e.handlerFuncName())
+			fmt.Fprintln(out, "\tctx := r.Context()") // context is always present as the first argument
+			fmt.Fprintln(out) // newline spacer
+			fmt.Fprintf(out, "\tparams := %s{}\n", paramsName)
 
 			for _, p := range e.Params {
-				// fmt.Println(p)
-
 				for _, f := range p.Fields {
 					if f.Type == "string" {
-						fmt.Printf("\t%s.%s = query.Get(\"%s\")\n", "params", f.Name, f.srcQueryParam())
+						fmt.Fprintf(out, "\t%s.%s = r.FormValue(\"%s\")\n", "params", f.Name, f.srcQueryParam())
 					} else {
 						// extra logic for 'int' handling
 						rawVarName := "raw" + f.Name
 						intVarName := strings.ToLower(f.Name) + "Int" // already converted
-						fmt.Printf("\t%s = query.Get(\"%s\")\n", rawVarName, f.srcQueryParam())
-						fmt.Printf("\tif len(%s) > 0 {\n", rawVarName)
-						fmt.Printf("\t\t%s, err := strconv.Atoi(%s)\n", intVarName, rawVarName)
-						fmt.Println("\t\tif err != nil {")
-						fmt.Printf("\t\t\tthrowBadRequest(w, \"%s must be integer\")\n", strings.ToLower(f.Name))
-						fmt.Println("\t\t\treturn")
-						fmt.Println("\t\t}")
+						fmt.Fprintf(out, "\t%s := r.FormValue(\"%s\")\n", rawVarName, f.srcQueryParam())
+						fmt.Fprintf(out, "\tif len(%s) > 0 {\n", rawVarName)
+						fmt.Fprintf(out, "\t\t%s, err := strconv.Atoi(%s)\n", intVarName, rawVarName)
+						fmt.Fprintln(out, "\t\tif err != nil {")
+						fmt.Fprintf(out, "\t\t\tthrowBadRequest(w, \"%s must be int\")\n", strings.ToLower(f.Name))
+						fmt.Fprintln(out, "\t\t\treturn")
+						fmt.Fprintln(out, "\t\t}")
 
-						fmt.Println("")
-						fmt.Printf("\t\tparams.%s = %s\n", f.Name, intVarName)
-						fmt.Println("\t}")
+						fmt.Fprintln(out, "")
+						fmt.Fprintf(out, "\t\tparams.%s = %s\n", f.Name, intVarName)
+						fmt.Fprintln(out, "\t}")
 					}
 				}
 
-				fmt.Println()
+				fmt.Fprintln(out)
 
 				// set default values
 				for _, f := range p.Fields {
@@ -369,71 +349,86 @@ func main() {
 						} else {
 							panic("unsupported type")
 						}
-						fmt.Printf("\tif %s.%s == %s {\n", "params", f.Name, zv)
-						fmt.Printf("\t\t%s.%s = \"%s\"\n", "params", f.Name, f.DefaultValue)
-						fmt.Println("\t}")
+						fmt.Fprintf(out, "\tif %s.%s == %s {\n", "params", f.Name, zv)
+						fmt.Fprintf(out, "\t\t%s.%s = \"%s\"\n", "params", f.Name, f.DefaultValue)
+						fmt.Fprintln(out, "\t}")
 					}
 				}
 
-				fmt.Println("")
-				fmt.Println("\terr := params.Validate()")
-				fmt.Println("\tif err != nil {")
-				fmt.Println("\t\tthrowBadRequest(w, err.Error())")
-				fmt.Println("\t\treturn")
-				fmt.Println("\t}")
+				fmt.Fprintln(out, "")
+				fmt.Fprintln(out, "\terr := params.Validate()")
+				fmt.Fprintln(out, "\tif err != nil {")
+				fmt.Fprintln(out, "\t\tthrowBadRequest(w, err.Error())")
+				fmt.Fprintln(out, "\t\treturn")
+				fmt.Fprintln(out, "\t}")
 
-				fmt.Println()
-				fmt.Println("\tsrvResponse, err := srv.Create(ctx, params)") // ?
-				fmt.Println("\tvar ar apiResponse")
-				fmt.Println("\tif err != nil {")
-				fmt.Println("\t\te := err.(ApiError)")
-				fmt.Println("\t\tw.WriteHeader(e.HTTPStatus)")
-				fmt.Println("\t\tar = apiResponse{e.Err.Error(), nil}")
-				fmt.Println("\t} else {")
-				fmt.Println("\t\tar = apiResponse{\"\", srvResponse}")
-				fmt.Println("\t}")
+				fmt.Fprintln(out)
+				fmt.Fprintf(out, "\tsrvResponse, err := srv.%s(ctx, params)\n", e.Name) // ?
+				fmt.Fprintln(out, "\tvar ar apiResponse")
+				fmt.Fprintln(out, "\tif err != nil {")
+				fmt.Fprintln(out, "\t\tswitch e := err.(type) {")
+				fmt.Fprintln(out, "\t\tcase ApiError:")
+				fmt.Fprintln(out, "\t\t\tw.WriteHeader(e.HTTPStatus)")
+				fmt.Fprintln(out, "\t\t\tar = apiResponse{e.Err.Error(), nil}")
+				fmt.Fprintln(out, "\t\tdefault:")
+				fmt.Fprintln(out, "\t\t\tw.WriteHeader(http.StatusInternalServerError)")
+				fmt.Fprintln(out, "\t\t\tar = apiResponse{e.Error(), nil}")
+				fmt.Fprintln(out, "\t\t}")
+				fmt.Fprintln(out, "\t} else {")
+				fmt.Fprintln(out, "\t\tar = apiResponse{\"\", srvResponse}")
+				fmt.Fprintln(out, "\t}")
 
-				fmt.Println()
-				fmt.Println("\tj, err := encodeJson(ar)")
-				fmt.Println("\tif err != nil {")
-				fmt.Println("\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)")
-				fmt.Println("\t\treturn")
-				fmt.Println("\t}")
-				fmt.Println("\tw.Write(j)")
-				fmt.Println("}")
+				fmt.Fprintln(out)
+				fmt.Fprintln(out, "\tj, err := encodeJson(ar)")
+				fmt.Fprintln(out, "\tif err != nil {")
+				fmt.Fprintln(out, "\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)")
+				fmt.Fprintln(out, "\t\treturn")
+				fmt.Fprintln(out, "\t}")
+				fmt.Fprintln(out, "\tw.Write(j)")
+				fmt.Fprintln(out, "}")
 			}
 		} // end of handler funcs
 
 		// generate the main mux 'ServerHTTP' functions
-		fmt.Printf("func (%s %s) ServeHTTP(w http.ResponseWriter, r *http.Request) {\n", muxPrefix, k)
-		fmt.Println("\tswitch r.URL.Path {")
+		fmt.Fprintf(out, "func (%s %s) ServeHTTP(w http.ResponseWriter, r *http.Request) {\n", muxPrefix, k)
+		fmt.Fprintln(out, "\tswitch r.URL.Path {")
 		for _, e := range v {
-			fmt.Printf("\tcase \"%s\":\n", e.Path)
-			fmt.Printf("\t\tf := %s.%s\n", muxPrefix, e.handlerFuncName())
-			fmt.Printf("\t\tf = requestMethodMiddleWare(f, \"%s\")\n", e.Method)
-			if e.Auth { // optional part
-				fmt.Println("\t\tf = authenticatedMiddleWare(f)")
+			fmt.Fprintf(out, "\tcase \"%s\":\n", e.Path)
+			fmt.Fprintf(out, "\t\tf := %s.%s\n", muxPrefix, e.handlerFuncName())
+			if e.Method != "" {
+				fmt.Fprintf(out, "\t\tf = requestMethodMiddleWare(f, \"%s\")\n", e.Method)
 			}
-			fmt.Println("\t\tf(w, r)")
+			if e.Auth { // optional part
+				fmt.Fprintln(out, "\t\tf = authenticatedMiddleWare(f)")
+			}
+			fmt.Fprintln(out, "\t\tf(w, r)")
 		}
 
-		fmt.Println("\tdefault:\n\t\thttp.NotFound(w, r)") // default case
-		fmt.Println("\t}")
-		fmt.Println("}")
+		fmt.Fprintln(out, "\tdefault:\n") // default case
+		fmt.Fprintln(out, "\t\tw.WriteHeader(http.StatusNotFound)")
+		fmt.Fprintln(out, "\t\tar := apiResponse{\"unknown method\", nil}")
+		fmt.Fprintln(out, "\t\tj, err := encodeJson(ar)")
+		fmt.Fprintln(out, "\t\tif err != nil {")
+		fmt.Fprintln(out, "\t\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)")
+		fmt.Fprintln(out, "\t\t\treturn")
+		fmt.Fprintln(out, "\t\t}")
+		fmt.Fprintln(out, "\t\tw.Write(j)")
+		fmt.Fprintln(out, "\t}")
+		fmt.Fprintln(out, "}")
 
 		// ---
 
 		for _, e := range v {
 			for _, p := range e.Params {
-				fmt.Printf("func (%s %s) Validate() error {\n", muxPrefix, p.Name)
+				fmt.Fprintf(out, "func (%s %s) Validate() error {\n", muxPrefix, p.Name)
 				for _, f := range p.Fields {
 					for _, v := range f.Validators {
-						fmt.Println(Indent(v.Render(f), "\t"))
+						fmt.Fprintln(out, Indent(v.Render(f), "\t"))
 					}
 				}
 
-				fmt.Println("\treturn nil")
-				fmt.Println("}")
+				fmt.Fprintln(out, "\treturn nil")
+				fmt.Fprintln(out, "}")
 			}
 		}
 	}
@@ -441,17 +436,17 @@ func main() {
 	// ---
 
 	// print headers
-	fmt.Println("type apiResponse struct {")
-	fmt.Println("\tError    string      `json:\"error\"`")
-	fmt.Println("\tResponse interface{} `json:\"response,omitempty\"`")
-	fmt.Println("}")
-	fmt.Println()
+	fmt.Fprintln(out, "type apiResponse struct {")
+	fmt.Fprintln(out, "\tError    string      `json:\"error\"`")
+	fmt.Fprintln(out, "\tResponse interface{} `json:\"response,omitempty\"`")
+	fmt.Fprintln(out, "}")
+	fmt.Fprintln(out)
 
-	fmt.Println(jsonFacilties()) // json
+	fmt.Fprintln(out, jsonFacilties()) // json
 
 	// add middlewares
-	fmt.Println(authenticatedMiddleware())
-	fmt.Println(requestMethodMiddleware())
+	fmt.Fprintln(out, authenticatedMiddleware())
+	fmt.Fprintln(out, requestMethodMiddleware())
 } // end of main func
 
 // ---
@@ -538,6 +533,8 @@ func throwBadRequest(w http.ResponseWriter, errorMessage string) {
 	w.Write(j) // flush
 }`
 }
+
+// ---
 
 // helper function to indent multiple (primarily) texts
 func Indent(input string, indenter string) string {
