378819de22a2da61dbda7b88819a78d2e259c0ed
Dmitri <unabl4@gmail.com>
2019-03-15 14:09:59 +0200

    initial handler structure; wip

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"database/sql"
+	"net/http"
+)
+
+type Handler struct {
+	DbConn *sql.DB
+}
+
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+func NewDbExplorer(db *sql.DB) (http.Handler, error) {
+	h := Handler{db} // new instance
+	return h, nil    // at the moment, not entirely sure what the error is for
+}
+
+// ---
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+}
