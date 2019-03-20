254766e85ec2cd110a01cea0305051162759d3ac
Dmitri <unabl4@gmail.com>
2019-03-20 16:23:46 +0200

    initial db request parser; wip

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Handler struct {
 	DB *sql.DB
-	// --- 
+	// ---
 	Tables []Table
 }
 
@@ -27,33 +28,72 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &h, nil    // at the moment, not entirely sure what the error is for
+	return &h, nil // at the moment, not entirely sure what the error is for
 }
 
 // ===
 
 func (h *Handler) Initialize() error {
-	var n string	// table name
+	var n string                        // table name
 	q, err := h.DB.Query("SHOW TABLES") // and not 'QueryRow'
 	if err != nil {
 		return err
 	}
-	 
+
 	defer h.DB.Close()
-	for q.Next() {	// loop through rows
+	for q.Next() { // loop through rows
 		err := q.Scan(&n)
 		if err != nil {
 			return err
 		}
-	
+
 		t := Table{n}
 		h.Tables = append(h.Tables, t)
 	}
 
-	return nil	// no errors
+	return nil // no errors
 }
 
 // ---
+
+type DbRequest struct {
+	TableName string // for what table
+	// optional params
+	RecordId *string // particular record we are after
+	Limit    *string
+	Offset   *string
+}
+
+// extract necessary params from the request url (table name, record id) path and query (limit, offset)
+func parseDbRequest(r *http.Request) DbRequest {
+	p := strings.Split(r.URL.Path, "/") // path components
+	q := r.URL.Query()                  // query components
+	d := DbRequest{}
+	var t []string
+	for _, h := range p { // filter out empty parts
+		if h != "" {
+			t = append(t, h)
+		}
+	}
+
+	if len(t) > 0 {
+		// any path params?
+		d.TableName = t[0]
+		if len(t) > 1 {
+			d.RecordId = &t[1]
+		}
+	}
+
+	if offset, ok := q["offset"]; ok {
+		d.Offset = &offset[0]
+	}
+
+	if limit, ok := q["limit"]; ok {
+		d.Limit = &limit[0]
+	}
+
+	return d
+}
 
 // primary router
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
