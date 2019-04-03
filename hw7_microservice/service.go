9ed0633b37dc6261465f7131375ca997143313ae
Dmitri <unabl4@gmail.com>
2019-04-03 15:59:45 +0300

    declare and implement service handlers, register them to the grpc server; perform ACL string descriptor parsing; fix the grpc server stop condition to mitigate premature termination; wip

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -5,13 +5,64 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	_ "fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
+type BizSrv struct {
+}
+
+// ---
+
+func (s BizSrv) Add(ctx context.Context, _ *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s BizSrv) Check(ctx context.Context, _ *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s BizSrv) Test(ctx context.Context, _ *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+// ---
+// streaming services
+
+type AdmSrv struct {
+}
+
+func (s AdmSrv) Logging(_ *Nothing, srv Admin_LoggingServer) error {
+	return nil // ?
+}
+
+func (s AdmSrv) Statistics(interval *StatInterval, srv Admin_StatisticsServer) error {
+	return nil
+}
+
+type ACL map[string][]string
+
+// ---
+// union type
+type Srv struct {
+	ctx context.Context // cancellation
+	acl ACL
+
+	BizSrv
+	AdmSrv
+}
+
+// ===
+
 func StartMyMicroservice(ctx context.Context, listenAddr string, aclData string) error {
+	var acl ACL
+	if err := json.Unmarshal([]byte(aclData), &acl); err != nil {
+		return err
+	}
+
 	lis, err := net.Listen("tcp", listenAddr) // tcp socket
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -20,17 +71,25 @@ func StartMyMicroservice(ctx context.Context, listenAddr string, aclData string)
 		grpc.UnaryInterceptor(unaryInterceptor),
 	)
 
-	// grpc server start logic
+	srv := &Srv{ctx: ctx, acl: acl}
+
+	// register the services
+	RegisterBizServer(grpcServer, srv)
+	RegisterAdminServer(grpcServer, srv) // streaming?
+
+	// ---
+
+	// grpc server start logic (non-blocking)
 	go func() {
 		err = grpcServer.Serve(lis)
 		if err != nil {
-			panic("grpc server start failed")
+			log.Fatal("grpc server start failed", err)
 		}
 	}()
 
 	// grpc server stop logic
 	go func() {
-		// ?
+		<-ctx.Done() // await until everything is done and the stop signal is acquired
 		grpcServer.Stop()
 	}()
 
@@ -40,6 +99,5 @@ func StartMyMicroservice(ctx context.Context, listenAddr string, aclData string)
 // ---
 
 func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	fmt.Println("TEST", info)
 	return handler(ctx, req)
 }
