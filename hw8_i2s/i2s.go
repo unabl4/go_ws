cebacafb4332a0dde3328656cb59d399877c62ff
Dmitri <unabl4@gmail.com>
2019-04-09 12:07:30 +0300

    wip

--- hw8_i2s/i2s.go
+++ hw8_i2s/i2s.go
@@ -2,52 +2,77 @@ package main
 
 import (
 	"errors"
-	_ "fmt"
+	"fmt"
 	"reflect"
 )
-
-type Simple2 struct {
-	ID       int
-	Username string
-	Active   bool
-}
 
 func i2s(in interface{}, out interface{}) error {
 	// todo
 	// fmt.Println("IN", in, "OUT", out)
 
 	v := reflect.ValueOf(out)
-	if v.Kind() != reflect.Ptr {	// ?
+	if v.Kind() != reflect.Ptr { // ?
 		// the input structure MUST be a pointer, PANIC otherwise
 		return errors.New("The input is not a pointer")
 	}
 
 	// ---
 
-	elem := v.Elem()	// pointer inner
+	inType := reflect.TypeOf(in).Kind()
+	inVal := reflect.ValueOf(in)
 
-	switch elem.Kind() {
+	// ---
+
+	outElem := v.Elem() // pointer inner
+
+	switch outElem.Kind() {
 	case reflect.Slice:
 		// we are given a slice (array)
 		// slice of what?
+		T := outElem.Type().Elem() // array -> single elem type
 
-		T := elem.Type().Elem()	// array -> single elem type
-		newRecord := reflect.New(T).Elem()	// create new of type of the slice
+		// if the 'out' = slice, then the 'in' must also be slice (to match)
+		if inType != reflect.Slice {
+			outElem.Set(reflect.Zero(outElem.Type())) // empty slice?
+			return errors.New("mismatching input and ouput types")
+		}
 
-		idField := newRecord.FieldByName("ID")
-		idField.SetInt(123)
+		// inval = slice => .Len() is available
+		for i := 0; i < inVal.Len(); i++ { // correct way to iterate
+			sliceElem := inVal.Index(i).Elem() // entry
+			if sliceElem.Kind() != reflect.Map {
+				panic("NO!") // should not happen, but still
+			}
 
-		usernameField := newRecord.FieldByName("Username")
-		usernameField.SetString("Test")
+			newRecord := reflect.New(T).Elem() // create new of type of the slice
 
-		activeField := newRecord.FieldByName("Active")
-		activeField.SetBool(false)
+			// change/populate the map 'record' keys
+			for _, mapKey := range sliceElem.MapKeys() {
+				keyName := mapKey.String()
+				field := newRecord.FieldByName(keyName) // get the ref in the 'out' structure
+				switch field.Kind() {
+				case reflect.Int: // ?; and not sure about 'float'
+					innerValue := sliceElem.MapIndex(mapKey).Elem()
+					field.SetInt(int64(innerValue.Float())) // float -> int trick
+				case reflect.String:
+					innerValue := sliceElem.MapIndex(mapKey).Elem()
+					field.SetString(innerValue.String())
+				case reflect.Bool:
+					innerValue := sliceElem.MapIndex(mapKey).Elem()
+					field.SetBool(innerValue.Bool())
+				default:
+					// not sure about this part, probably recursion
+					fmt.Println("default")
+				}
+			}
 
-		elem.Set(reflect.Append(elem, newRecord))	// add elem to struct
+			// add the new element/entry/record to the output slice
+			outElem.Set(reflect.Append(outElem, newRecord)) // add elem to struct
+		}
 	}
 
 	// ---
-	
+
 	// if elem.Kind() == reflect.Struct {
 	// 	fmt.Println("STRUCT IT IS")
 	// 	for i := 0; i < elem.NumField(); i++ {
